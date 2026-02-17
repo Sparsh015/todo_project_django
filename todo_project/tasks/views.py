a42a3322e5b2694b669257dfb5218cbaa4e31db7
sparsh15 <sparsh.1518@gmail.com>
2026-02-18 01:09:20 +0530

    templates and list_tasks and add task view completed

--- todo_project/tasks/views.py
+++ todo_project/tasks/views.py
@@ -3,8 +3,21 @@ from .models import Task
 
 # Create your views here.
 
-def task_list(request){
+def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/task_list.html', {'tasks' : tasks})
+from .models import Task
 
-}
+def add_task(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+
+        Task.objects.create(
+            title=title,
+            description=description
+        )
+
+        return redirect('task_list')
+
+    return render(request, 'tasks/add_task.html')

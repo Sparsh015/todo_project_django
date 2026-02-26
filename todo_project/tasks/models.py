3c727758d98dbf50a781fb50a8eaee3e1b0704e4
sparsh15 <sparsh.1518@gmail.com>
2026-02-26 23:28:32 +0530

    edit task(on same form of add task) authentication initial signup and login views

--- todo_project/tasks/models.py
+++ todo_project/tasks/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     priority_choices = [
         ('Low', 'Low'),
         ('Medium', 'Medium'),
--- todo_project/tasks/views.py
+++ todo_project/tasks/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
 # Create your views here.
 
+@login_required
 def task_list(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter(user = request.user)
     return render(request, 'tasks/task_list.html', {'tasks' : tasks})
 from .models import Task
 
 def add_task(request):
+    
     if request.method == "POST":
         title = request.POST.get('title')
         description = request.POST.get('description')
         priority=request.POST.get('priority')
         due_date=request.POST.get('due_date')
         Task.objects.create(
+            user = request.user,
             title=title,
             description=description,
             priority=priority,
@@ -37,9 +43,6 @@ def delete_task(request, task_id):
         
     return redirect('task_list')
 
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Task
-
 
 def edit_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
@@ -56,3 +59,25 @@ def edit_task(request, task_id):
         return redirect('task_list')
 
     return render(request, 'tasks/add_task.html', {'task': task})
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = User.objects.create_user(username=username, password=password)
+        login(request, user)
+        return redirect('task_list')
+    return render(request, 'tasks/signup.html')
+
+def logout_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('task_list')
+    return redirect(request, 'tasks/login.html')
+
+
